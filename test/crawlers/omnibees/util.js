fe6f1ad713616222d8972a4feb2d193a812742d9
Isaias Vallejos <isaiasalansv@hotmail.com>
2019-04-26 19:15:57 -0300

    Add crawler omnibees test util - Add date constants

---
+++ test/crawlers/omnibees/util.js
@@ -0,0 +1,13 @@
+import { format, addDays, subDays } from 'date-fns'
+import { VALIDATION_DATE_FORMAT } from 'crawlers/omnibees/schema/keywords'
+
+export const now = new Date()
+export const yesterday = subDays(now, 1)
+export const tomorrow = addDays(now, 1)
+export const afterTomorrow = addDays(now, 2)
+export const formatedYesterday = format(yesterday, VALIDATION_DATE_FORMAT)
+export const formatedTomorrow = format(tomorrow, VALIDATION_DATE_FORMAT)
+export const formatedAfterTomorrow = format(
+  afterTomorrow,
+  VALIDATION_DATE_FORMAT
+)
