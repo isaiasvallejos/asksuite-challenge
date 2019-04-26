bb6bf827f103c69f0c59793715b37ca88b04cda9
Isaias Vallejos <isaiasalansv@hotmail.com>
2019-04-26 19:17:22 -0300

    Add schema validation module

---
+++ src/vendor/json/validator/index.js
@@ -0,0 +1,22 @@
+import { assoc } from 'ramda'
+import { curryAsync } from 'util/ramda'
+
+import Ajv from 'ajv'
+
+// createValidator :: Ajv.Validator
+export const createValidator = () =>
+  new Ajv({
+    $data: true,
+    allErrors: true,
+    jsonPointers: true
+  })
+
+// compileSchema :: Ajv.Validator -> Ajv.Schema -> Object -> Promise<Object>
+export const compileSchema = curryAsync(async (validator, schema, data) => {
+  try {
+    const compile = validator.compile({ $async: true, ...schema })
+    return await compile(data)
+  } catch (error) {
+    throw assoc('code', 'EVALIDATION', error)
+  }
+})
