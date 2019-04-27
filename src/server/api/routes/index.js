25e6ba1611035f4589a4e224bff9c33d6958dceb
Isaias Vallejos <isaiasalansv@hotmail.com>
2019-04-27 04:13:02 -0300

    Add simple search route with default parameters

--- src/server/api/routes/index.js
+++ src/server/api/routes/index.js
@@ -1,5 +1,20 @@
 import express from 'express'
+import { getHotelsBySearch } from 'crawlers/omnibees/hotels'
+import { getBody } from 'util/server/api/requests'
+import { responseWithDataAndSuccess } from 'util/server/api/responses'
 
 const router = express.Router()
+
+router.post('/search', (request, response, next) => {
+  const search = getBody(request)
+
+  getHotelsBySearch(
+    process.env.DEFAULT_OMNIBEES_URI,
+    process.env.DEFAULT_OMNIBEES_CLIENT_ID,
+    search
+  )
+    .then(responseWithDataAndSuccess(response, next))
+    .catch(next)
+})
 
 export default router
