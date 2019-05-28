a72c056ffa4c47327159f3f73d49e0d8b4aa0146
Asksuite <asksuite@asksuite.com>
2019-05-28 17:54:22 -0300

    Fix documentation and add crawl id property

--- src/crawlers/omnibees/rooms/props.js
+++ src/crawlers/omnibees/rooms/props.js
@@ -1,29 +1,35 @@
 import { prop, assoc, map, compose } from 'ramda'
 import { removeQueryParameterFromUrl } from 'util/query-string'
 
-// getRestricted :: Omnibees.Hotel -> Boolean
+// getCrawlId :: Omnibees.Room -> String
+export const getCrawlId = prop('crawlId')
+
+// getRestricted :: Omnibees.Room -> Boolean
 export const getRestricted = prop('restricted')
 
-// getImages :: Omnibees.Hotel -> String[]
+// getImages :: Omnibees.Room -> String[]
 export const getImages = prop('images')
 
-// getPrice :: Omnibees.Hotel -> Number
+// getPrice :: Omnibees.Room -> Number
 export const getPrice = prop('price')
 
-// getDetailsUrl :: Omnibees.Hotel -> String
+// getDetailsUrl :: Omnibees.Room -> String
 export const getDetailsUrl = prop('detailsUrl')
 
-// setRestricted :: Boolean -> Omnibees.Hotel
+//setCrawlId :: Omnibees.Room -> String
+export const setCrawlId = assoc('crawlId')
+
+// setRestricted :: Boolean -> Omnibees.Room
 export const setRestricted = assoc('restricted')
 
-// setImages :: String[] -> Omnibees.Hotel
+// setImages :: String[] -> Omnibees.Room
 export const setImages = compose(
   assoc('images'),
   map(removeQueryParameterFromUrl('sz'))
 )
 
-// setPrice :: Number -> Omnibees.Hotel
+// setPrice :: Number -> Omnibees.Room
 export const setPrice = assoc('price')
 
-// setDetailsUrl :: String -> Omnibees.Hotel
+// setDetailsUrl :: String -> Omnibees.Room
 export const setDetailsUrl = assoc('detailsUrl')
